a227a0f3d8e114789feb360e81bfc886543bf0b6
Jay Chen <richterplus@hotmail.com>
2017-06-03 19:19:57 +0800

    Create project.

---
+++ mybatis-q-maven-plugin/src/main/java/com/github/mybatisq/GenCodeMojo.java
@@ -0,0 +1,15 @@
+package com.github.mybatisq;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "gencode")
+public class GenCodeMojo extends AbstractMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		
+	}
+}
